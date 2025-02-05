2cbc4946bbbee912daab2262b43a048cfe14746d
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-02-05 22:56:25 +0800

    Update evaluate_first_frame_constrained.py

--- evaluate_first_frame_constrained.py
+++ evaluate_first_frame_constrained.py
@@ -3,7 +3,7 @@ import os
 import torch
 from torchvision.io import write_video
 from torchvision.utils import save_image
-
+from pathlib import Path
 from options.test_options import TestOptions
 from models.models import create_model
 
@@ -23,7 +23,15 @@ input_constrained_frames_dir/
     ├── 0.png
     ├── 10.png
     └── ...
+
+改变思路:
+1. 遍历每个sub_dir 然后以sub_dir 里面的每个constrain frame 来生成后续的frame.
+2. PS: 这里可以asset一下，这里面的sub_directory 的sep 是和传入的args是一样的。    
 """
+
+
+def sort_by_file_name_int(file_paths: list):
+    return sorted(file_paths, key=lambda x: int(x.stem))
 
 
 class FrameConstrainedOptions(TestOptions):
@@ -71,7 +79,20 @@ def test():
 
     os.makedirs(opt.results_dir, exist_ok=True)
 
+    constrained_frames_dir = Path(opt.input_constrained_frames_dir)
+
     with torch.no_grad():
+
+        for sub_dir in constrained_frames_dir.iterdir():
+            if sub_dir.is_dir():
+                constrained_image_paths = list(sub_dir.glob("*.png"))  # 只获取PNG文件
+                sorted_constrained_image_paths = sort_by_file_name_int(
+                    constrained_image_paths
+                )
+                for constrained_image_path in sorted_constrained_image_paths:
+                    ## Infer here!!!
+                    raise NotImplementedError
+
         for j in range(opt.num_test_videos):
             z.data.normal_()
             prefix = (
